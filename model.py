e3c49576fc061ed844d21ed9b2c88550271de206
codydegen <codydegen@gmail.com>
2020-03-23 12:09:26 -0400

    Allow multiple iterations of bracket to run

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import pandas as pd
 from anytree import Node, RenderTree, NodeMixin, AsciiStyle
 from anytree.exporter import DotExporter
 import os
+import copy
+import time
 
 final_four_pairings = {
   "mens" : {
@@ -36,17 +38,50 @@ seed_pairings = [[1,16],
                  [2,15]]
 
 class Bracket:
-  def __init__(self, gender='mens', year=2019, number_runs=1):
+  def __init__(self, gender='mens', year=2019, number_simulations=1):
     self.gender = gender
     self.year = year 
     self.all_teams = {}
     self.start_bracket = None
+    self.running_bracket = None
     self.game_pairing = 0
-    self.number_runs = number_runs
+    self.number_simulations = number_simulations
     self.completed_simulations = 0
     pass
 
+  def reset_bracket(self):
+    self.running_bracket=copy.deepcopy(self.start_bracket)
+    pass
 
+  def batch_simulate(self):
+    for i in range(0, self.number_simulations-1):
+      self.simulate_bracket()
+      self.update_winners()
+      self.reset_bracket()
+      
+      self.completed_simulations += 1
+      if self.completed_simulations%100 == 0:
+        print("simulation number "+str(i+1)+" completed.")
+
+  def update_winners(self):
+    # for region in all_teams:
+    #   for seed in region:
+    #     for team in seed:
+    # for game in self.running_bracket.descendants:
+    #   winner = game.winner
+    #   teams = self.all_teams[winner.region][winner.seed]
+    #   if len(teams)==1:
+    #     team = teams[0]
+    #   else:
+    #     if teams[0].name == winner.name:
+    #       team = teams[0]
+    #     else:
+    #       team = teams[1]
+    #   for round in winner.wins:
+    #     if winner.wins[round] > 0:
+    #       team.wins[round] += 1
+    #       pass
+      pass
 
   def create_teams(self):
     path = "data/"+str(self.year)+"_all_prepped_data.csv"
@@ -57,7 +92,7 @@ class Bracket:
     else:
       print(" couldn't find data")
       # TBD, hook up data scraping to this
-      a = prep_data(path)
+      a = self.prep_data(path)
 
     team_data = pd.read_csv(path)
     gender_specific = team_data[team_data.gender == self.gender]
@@ -94,6 +129,7 @@ class Bracket:
     for ff_pairings in final_four_pairings[self.gender][self.year]:
       finals.add_child(self.add_semis(ff_pairings))
     self.start_bracket = finals
+    self.running_bracket = copy.deepcopy(self.start_bracket)
     pass
     
   
@@ -133,7 +169,7 @@ class Bracket:
 
   
   def simulate_bracket(self):
-    self.simulate_bracket_recursion(self.start_bracket)
+    self.simulate_bracket_recursion(self.running_bracket)
     pass
   
   def simulate_bracket_recursion(self, node):
@@ -149,9 +185,9 @@ class Bracket:
 
 
 class Team:
-  def __init__(self, name, rank, region, elo, picked_frequency):
+  def __init__(self, name, seed, region, elo, picked_frequency):
     self.name = name
-    self.rank = rank
+    self.seed = seed
     self.region = region
     self.elo = elo
     self.picked_frequency = picked_frequency
@@ -168,11 +204,11 @@ class Team:
     pass
 
   def __str__(self):
-    return str(self.rank)+" "+str(self.name)+ " "+str(self.elo)
+    return str(self.seed)+" "+str(self.name)+ " "+str(self.elo)
 
   def __repr__(self):
-    # return "Name: "+str(self.name)+"\nRank: "+str(self.rank)+"\nRegion: "+str(self.region)+"\nRating: "+str(self.elo)
-    return str(self.rank)+" "+str(self.name)+ " "+str(self.elo)
+    # return "Name: "+str(self.name)+"\nseed: "+str(self.seed)+"\nRegion: "+str(self.region)+"\nRating: "+str(self.elo)
+    return str(self.seed)+" "+str(self.name)+ " "+str(self.elo)
 
   def update_elo(self, team_two, winner):
     # update elo for future rounds based on game outcome. not sure how to do this for now so no update to elo will occur
@@ -216,7 +252,7 @@ class NodeGame(Game, NodeMixin):
     assert not self.winner, "game between "+str(self.team_one)+" and  "+str(self.team_two)+" already has been played."
 
     team_one_chance = 1.0/(1.0 + pow(10,(-(self.team_one.elo-self.team_two.elo))*30.464/400))
-    print(self.team_one.name+"'s chance of winning vs "+self.team_two.name+" = "+str(math.trunc(team_one_chance*100))+"%")
+    # print(self.team_one.name+"'s chance of winning vs "+self.team_two.name+" = "+str(math.trunc(team_one_chance*100))+"%")
     if random.random() < team_one_chance:
       # team one wins
       self.winner = self.team_one
@@ -229,7 +265,26 @@ class NodeGame(Game, NodeMixin):
       self.team_two.update_elo(self.team_one, True)
     # pass
     self.update_bracket(self.winner)
-    self.winner.wins[self.round_num] += 1
+    self.update_wins(bracket, self.winner, self.round_num)
+    # self.winner.wins[self.round_num] += 1
+
+  def update_wins(self, bracket, winner, round_number):
+    #     for region in all_teams:
+    #   for seed in region:
+    #     for team in seed:
+    # for game in self.running_bracket.descendants:
+    #   winner = game.winner
+    teams = bracket.all_teams[winner.region][winner.seed]
+    if len(teams)==1:
+      team = teams[0]
+    else:
+      if teams[0].name == winner.name:
+        team = teams[0]
+      else:
+        team = teams[1]
+    team.wins[round_number] += 1
+    # bracket.
+    pass
 
   def update_bracket(self, winning_team):
     # update bracket with results of game
@@ -252,10 +307,14 @@ class NodeGame(Game, NodeMixin):
       child.parent = self
     pass
     
-bracket = Bracket()
+t=time.time()
+bracket = Bracket(number_simulations = 1000)
 bracket.create_teams()
 bracket.create_bracket()
+bracket.batch_simulate()
+t = time.time() - t
 print(RenderTree(bracket.start_bracket, style=AsciiStyle()))
-bracket.simulate_bracket()
-print(RenderTree(bracket.start_bracket, style=AsciiStyle()))
+print(t)
+print(RenderTree(bracket.running_bracket, style=AsciiStyle()))
+bracket.reset_bracket()
 
