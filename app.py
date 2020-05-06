7554422697daf4d81834f95927bb3f107da5ec86
codydegen <codydegen@gmail.com>
2020-05-06 12:12:32 -0400

    Add preliminary callback function

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 def get_array_from_dataframe(frame, array_type, data_type):
     return frame[frame['name']==data_type][array_type].values[0]
 
-def prepare_ranks_graph(results):
-    most_valuable_rank = get_array_from_dataframe(results, 'ranks', 'most_valuable_teams')
-    most_popular_rank = get_array_from_dataframe(results, 'ranks', 'most_popular_teams')
-    chalk_rank = get_array_from_dataframe(results, 'ranks', 'chalk')
+def prepare_ranks_graph(entry_results, special_results):
+    most_valuable_rank = get_array_from_dataframe(special_results, 'ranks', 'most_valuable_teams')
+    most_popular_rank = get_array_from_dataframe(special_results, 'ranks', 'most_popular_teams')
+    chalk_rank = get_array_from_dataframe(special_results, 'ranks', 'chalk')
 
     hist_data = [most_valuable_rank, most_popular_rank, chalk_rank]
     group_labels = ['Most Valuable Teams', 'Most Popular Teams', 'Chalk']
@@ -42,11 +42,11 @@ def prepare_ranks_graph(results):
     )
     return graph
 
-def prepare_scores_graph(results):
-    overall_winning_score_values = get_array_from_dataframe(all_results, 'simulations', 'winning_score')
-    chalk_values = get_array_from_dataframe(results, 'simulations', 'chalk')
-    most_valuable_values = get_array_from_dataframe(results, 'simulations', 'most_valuable_teams')
-    most_popular_values = get_array_from_dataframe(results, 'simulations', 'most_popular_teams')
+def prepare_scores_graph(entry_results, special_results):
+    overall_winning_score_values = get_array_from_dataframe(special_results, 'simulations', 'winning_score')
+    chalk_values = get_array_from_dataframe(special_results, 'simulations', 'chalk')
+    most_valuable_values = get_array_from_dataframe(special_results, 'simulations', 'most_valuable_teams')
+    most_popular_values = get_array_from_dataframe(special_results, 'simulations', 'most_popular_teams')
 
     hist_data = [overall_winning_score_values, chalk_values, most_valuable_values, most_popular_values]
     group_labels = ['Winning Score', 'Chalk', 'Most Valuable', 'Most Popular']
@@ -59,7 +59,7 @@ def prepare_scores_graph(results):
     )
     return graph
 
-def prepare_table(results):
+def prepare_table(entry_results, special_results):
 
     def get_placings(place, inclusive=False, percentile=False, average=False):
 
@@ -68,7 +68,7 @@ def prepare_table(results):
             if average:
                 return round(np.average(data_set),1)
             if percentile:
-                place = math.ceil(place/100*(len(results)-4))
+                place = math.ceil(place/100*(len(entry_results)))
             
             for score in data_set:
                 if score>place:
@@ -79,7 +79,7 @@ def prepare_table(results):
                     i+=1
                 elif score==place:
                     i+=1
-            return i
+            return str(round(i/number_simulations*100, 1))+"%"
             
 
         score_array = []
@@ -88,11 +88,11 @@ def prepare_table(results):
         score_array.append(get_sub_placings(chalk_rank, place, inclusive, percentile, average))
         return score_array
 
-    most_valuable_rank = get_array_from_dataframe(results, 'placings', 'most_valuable_teams')
+    most_valuable_rank = get_array_from_dataframe(special_results, 'placings', 'most_valuable_teams')
     most_valuable_rank.sort()
-    most_popular_rank = get_array_from_dataframe(results, 'placings', 'most_popular_teams')
+    most_popular_rank = get_array_from_dataframe(special_results, 'placings', 'most_popular_teams')
     most_popular_rank.sort()
-    chalk_rank = get_array_from_dataframe(results, 'placings', 'chalk')
+    chalk_rank = get_array_from_dataframe(special_results, 'placings', 'chalk')
     chalk_rank.sort()
     data = {
         'Bracket Type' : {
@@ -114,7 +114,8 @@ def prepare_table(results):
     average = get_placings(0, average=True)
     cells = dict(values=[bracket_type, first_places, second_places, third_places, top_fives, top_tens, top_five_percentile, average],
                 align=['center','center'],
-                height=30,)
+                height=30,
+                )
     # figure_data = df.from_dict(data, orient='index', columns=columns)
 
     figure = go.Figure(data=[go.Table(header=columns, 
@@ -124,12 +125,32 @@ def prepare_table(results):
         id='scoring-table',
         figure=figure
     )
-    return graph
+    return figure
 
+def prepare_number_teams_input():
+    teams_input = dcc.Input(
+        id='number-teams-input',
+        type='number',
+        value=number_teams,
+        max=number_teams,
+        min=0
+    )
+    return teams_input 
+
+@app.callback(
+    Output(component_id='scoring-table', component_property='figure'),
+    [Input(component_id='number-teams-input', component_property='value')]
+)
+def update_table(input_value):
+    filtered_dataframe = random_subsample(input_value)
+    print(input_value)
+    # filtered_dataframe['']
+    return prepare_table(filtered_dataframe, special_results)
 
 if __name__ == '__main__':
     # model.main()
-    number_simulations = 200
+    number_simulations = 20
+    number_teams = 100
     m = model.Model(number_simulations=number_simulations, gender="mens", scoring_sys="ESPN")
     m.batch_simulate()
     print("sims done")
@@ -137,7 +158,7 @@ if __name__ == '__main__':
     m.update_entry_picks()
     m.initialize_special_entries()
     m.analyze_special_entries()
-    m.add_bulk_entries_from_database(17)
+    m.add_bulk_entries_from_database(number_teams)
     m.add_simulation_results_postprocessing()
 
 
@@ -146,10 +167,14 @@ if __name__ == '__main__':
     special_wins = m.get_special_wins()
     special_results = all_results[-4:]
     entry_results = all_results[:-4]
-
+    sub_results = random_subsample(number_teams)
     figures = []
-    figures.append(prepare_ranks_graph(all_results))
-    figures.append(prepare_table(all_results))
-    figures.append(prepare_scores_graph(all_results))
+    figures.append(prepare_ranks_graph(sub_results, special_results))
+    figures.append(prepare_number_teams_input())
+    figures.append(dcc.Graph(
+        id='scoring-table',
+        figure=prepare_table(sub_results, special_results)))
+    figures.append(prepare_scores_graph(sub_results, special_results))
+
     app.layout = html.Div(figures)
     app.run_server(debug=True)
